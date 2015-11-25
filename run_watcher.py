5a949fe668a29b1e38eac79aa3fc745ca551cdd0
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2015-11-25 14:40:19 +0800

    add mount point detection feature. change default log searching path

--- run_watcher.py
+++ run_watcher.py
@@ -12,11 +12,13 @@ FLAG_DETECTION_USB = False
 FLAG_DETECTION_INSERT = False
 FLAG_DETECTION_UHCI = False
 FLAG_DETECTION_XHCI = False
+FLAG_MOUNT_DEVICE = False
 #FLAG_WHILE_LOOP = True
 
 def callback(filename, lines):
     global FLAG_DETECTION_USB, FLAG_DETECTION_INSERT
     global FLAG_DETECTION_UHCI, FLAG_DETECTION_XHCI
+    global FLAG_MOUNT_DEVICE
     global FLAG_WHILE_LOOP
     for line in lines:
         line_str = str(line)
@@ -24,8 +26,10 @@ def callback(filename, lines):
         if detect_str(line_str, 'uhci'): FLAG_DETECTION_UHCI = True
         if detect_str(line_str, 'xhci'): FLAG_DETECTION_XHCI = True
         if detect_str(line_str, 'USB Mass Storage device detected'): FLAG_DETECTION_INSERT = True
+        if detect_str(line_str, 'sdb'): FLAG_MOUNT_DEVICE = "sdb"
     if FLAG_DETECTION_USB and FLAG_DETECTION_INSERT and FLAG_DETECTION_UHCI:
         print("An USB mass storage was inserted in a uhci controller")
+        print("sdb")
         # stop the watcher loop
         #FLAG_WHILE_LOOP = False
         sys.exit()
@@ -41,7 +45,7 @@ def detect_str(line, str_2_detect):
     return False
 
 
-watcher = LogWatcher("./", callback, extensions=["plano_watcher"])
+watcher = LogWatcher("../", callback, extensions=["plano_watcher"])
 
 #while FLAG_WHILE_LOOP:
 watcher.loop()
