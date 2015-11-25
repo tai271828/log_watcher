2157a9c61ed33575751a6ecb83dbdd083045c8fb
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2015-11-25 14:32:40 +0800

    add uhci detection support so we could test it in kvm

--- run_watcher.py
+++ run_watcher.py
@@ -9,20 +9,28 @@ from watcher import *
 import sys
 
 FLAG_DETECTION_USB = False
-FLAG_DETECTION_XHCI = False
 FLAG_DETECTION_INSERT = False
+FLAG_DETECTION_UHCI = False
+FLAG_DETECTION_XHCI = False
 #FLAG_WHILE_LOOP = True
 
 def callback(filename, lines):
-    global FLAG_DETECTION_USB, FLAG_DETECTION_XHCI, FLAG_DETECTION_INSERT
+    global FLAG_DETECTION_USB, FLAG_DETECTION_INSERT
+    global FLAG_DETECTION_UHCI, FLAG_DETECTION_XHCI
     global FLAG_WHILE_LOOP
     for line in lines:
         line_str = str(line)
         if detect_str(line_str, 'USB'): FLAG_DETECTION_USB = True
+        if detect_str(line_str, 'uhci'): FLAG_DETECTION_UHCI = True
         if detect_str(line_str, 'xhci'): FLAG_DETECTION_XHCI = True
         if detect_str(line_str, 'USB Mass Storage device detected'): FLAG_DETECTION_INSERT = True
-    if FLAG_DETECTION_USB and FLAG_DETECTION_XHCI and FLAG_DETECTION_INSERT:
-        print("An Superspeed USB mass storage was inserted in a xhci controller")
+    if FLAG_DETECTION_USB and FLAG_DETECTION_INSERT and FLAG_DETECTION_UHCI:
+        print("An USB mass storage was inserted in a uhci controller")
+        # stop the watcher loop
+        #FLAG_WHILE_LOOP = False
+        sys.exit()
+    if FLAG_DETECTION_USB and FLAG_DETECTION_INSERT and FLAG_DETECTION_XHCI:
+        print("An USB mass storage was inserted in a xhci controller")
         # stop the watcher loop
         #FLAG_WHILE_LOOP = False
         sys.exit()
