e740b2a6c5e2a100e934a416eef1ddb41e003479
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2016-01-20 15:22:19 +0800

    add correct copyright and license info

--- run_watcher.py
+++ run_watcher.py
@@ -3,11 +3,29 @@
 Real-time log files watcher supporting log rotation.
 Works with Python >= 2.6 and >= 3.2, on both POSIX and Windows.
 
-This code is based on Giampaolo Rodola' <g.rodola [AT] gmail [DOT] com> 's code.
 
-Author: Taihsiang Ho (tai271828)
 License: MIT
 
+Original work Copyright (c) Giampaolo Rodola' <g.rodola [AT] gmail [DOT] com>
+Modified work Copyright (c) 2015-2016: Taihsiang Ho (tai271828) <tai271828@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
 
